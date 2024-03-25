b8aaca698abd6c4840c8950309e7ed5a302933db
Vincent <vincentnjogu611@gmail.com>
2024-03-25 19:20:11 +0300

    Arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
-
 // Write your code here
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2
+console.log (multiply) //outcome to be 62
+
+let min = 1;
+let max = 20;
+let random = Math.floor(Math.random() * (max - min + 1)) + min;
+console.log(random);
+
+
+let num3 = 20;
+let num4 = 5;
+let mod = (num3 / num4);
+console.log(mod); // Output will be 4
+
+
+
+
